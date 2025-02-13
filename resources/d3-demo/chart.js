799eaf8a8bfe3b0f00a6712b320d0f3224679b69
Nicholas Vega <122232969+nickmvega@users.noreply.github.com>
2025-02-13 19:28:29 +0000

    Pending changes exported from your codespace

--- resources/d3-demo/chart.js
+++ resources/d3-demo/chart.js
@@ -12,31 +12,61 @@ let svgScatter = d3.select("#scatterplot-container").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-let svgBarRoot = d3.select("#bar-container").append("svg")
-        .attr("width", svgWidth)
-        .attr("height", svgHeight),
-    svgBar = svgBarRoot.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
-    svgBarOverlay = svgBarRoot.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+let svgHistogramHP = d3.select("#histogram-hp-container").append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-let xScaleScatter,
+console.log("HP histogram container:", document.getElementById("histogram-hp-container"));
+console.log("HP SVG created:", svgHistogramHP.node());
+
+let svgHistogramMPG = d3.select("#histogram-mpg-container").append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+console.log("MPG histogram container:", document.getElementById("histogram-mpg-container"));
+console.log("MPG SVG created:", svgHistogramMPG.node());
+
+let brush = d3.brush() 
+    .on("start brush", brushFxn)
+    .on("end", brushFxn);
+
+let histogramHPData = [],
+    histogramMPGData = [],
+    filteredHistogramHPData = [],
+    filteredHistogramMPGData = [],
+    xScaleHistHP,
+    xScaleHistMPG;
+
+let scatterData = [], 
+    points, 
+    xScaleScatter,
     yScaleScatter,
     xScaleBar,
     yScaleBar;
+
+function getHistogramData(data, key, bins) {
+    const histogram = d3.histogram()
+        .value(d => d[key])
+        .domain(d3.extent(data, d => d[key]))
+        .thresholds(bins);
+    
+    return histogram(data);
+}
 
 d3.csv("cars.csv")
     .then(function (data) {
         console.log(data);
 
         // cast strings as numbers
-        for (let i = 0; i < data.length; i++) {
-            data[i].hp = +data[i].hp;
-            data[i].mpg = +data[i].mpg;
+        scatterData = deepCopy(data); 
+        for (let i = 0; i < scatterData.length; i++) {
+            scatterData[i].hp = +scatterData[i].hp;
+            scatterData[i].mpg = +scatterData[i].mpg;
         }
-
-        // reformat data
-        let barData = getBarData(data);
         
         // scatterplot:
         // create scales
@@ -67,52 +97,98 @@ d3.csv("cars.csv")
             .text("Miles per gallon")
 
         // plot data
-        svgScatter.selectAll("circle")
-            .data(data)
+        points = svgScatter.selectAll("circle") 
+            .data(scatterData) 
             .join("circle")
             .attr("cx", (d) => xScaleScatter(d.hp))
             .attr("cy", (d) => yScaleScatter(d.mpg))
             .attr("r", 5)
             .attr("class", "non-brushed");
 
+        // brush
+        svgScatter.append("g") 
+            .call(brush);
 
+        //histograms
+        histogramHPData = getHistogramData(data, 'hp', 20);
+        histogramMPGData = getHistogramData(data, 'mpg', 20);
 
-        // bar chart:
-        // set up scales
-        xScaleBar = d3.scaleBand()
-            .domain(barData.map((d) => d.cyl))
-            .range([0, width])
-            .padding(0.1);
-        yScaleBar = d3.scaleLinear()
-            .domain([0, d3.max(barData, (d) => d.count)])
+        xScaleHistHP = d3.scaleLinear()
+            .domain([d3.min(histogramHPData, d => d.x0), d3.max(histogramHPData, d => d.x1)])
+            .range([0, width]);
+            
+        let yScaleHistHP = d3.scaleLinear()
+            .domain([0, d3.max(histogramHPData, d => d.length)])
             .range([height, 0]);
 
-        // axes
-        let xAxisBar = svgBar.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(xScaleBar));
-        let yAxisBar = svgBar.append("g")
-            .call(d3.axisLeft(yScaleBar));
+        xScaleHistMPG = d3.scaleLinear()
+            .domain([d3.min(histogramMPGData, d => d.x0), d3.max(histogramMPGData, d => d.x1)])
+            .range([0, width]);
+            
+        let yScaleHistMPG = d3.scaleLinear()
+            .domain([0, d3.max(histogramMPGData, d => d.length)])
+            .range([height, 0]);
 
-        // label our axes
-        xAxisBar.append("text")
+        // Axes HP 
+        let xAxisHistHP = svgHistogramHP.append("g")
+            .attr("class", "axis")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(xScaleHistHP));
+            
+        let yAxisHistHP = svgHistogramHP.append("g")
+            .attr("class", "axis")
+            .call(d3.axisLeft(yScaleHistHP));
+
+        // Axes MPG
+        let xAxisHistMPG = svgHistogramMPG.append("g")
+            .attr("class", "axis")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(xScaleHistMPG));
+            
+        let yAxisHistMPG = svgHistogramMPG.append("g")
+            .attr("class", "axis")
+            .call(d3.axisLeft(yScaleHistMPG));
+
+        // Labels
+        xAxisHistHP.append("text")
             .attr("class", "label")
             .attr("transform", `translate(${width / 2}, 40)`)
-            .text("Cylinders")
-        yAxisBar.append("text")
+            .text("Horsepower");
+            
+        yAxisHistHP.append("text")
             .attr("class", "label")
-            .attr("transform", `translate(-40, ${2 * height / 5}) rotate(-90)`)
-            .text("Number of records")
+            .attr("transform", `translate(-40, ${height/2}) rotate(-90)`)
+            .text("Frequency");
 
-        // render bars
-        svgBar.selectAll("rect")
-            .data(barData)
+        xAxisHistMPG.append("text")
+            .attr("class", "label")
+            .attr("transform", `translate(${width / 2}, 40)`)
+            .text("Miles per Gallon");
+            
+        yAxisHistMPG.append("text")
+            .attr("class", "label")
+            .attr("transform", `translate(-40, ${height/2}) rotate(-90)`)
+            .text("Frequency");
+
+        // Draw HP histogram bars
+        svgHistogramHP.selectAll("rect")
+            .data(histogramHPData)
             .join("rect")
             .attr("class", "non-brushed")
-            .attr("x", (d) => xScaleBar(d.cyl))
-            .attr("y", (d) => yScaleBar(d.count))
-            .attr("width", xScaleBar.bandwidth())
-            .attr("height", (d) => height - yScaleBar(d.count));
+            .attr("x", d => xScaleHistHP(d.x0))
+            .attr("y", d => yScaleHistHP(d.length))
+            .attr("width", d => xScaleHistHP(d.x1) - xScaleHistHP(d.x0))
+            .attr("height", d => height - yScaleHistHP(d.length));
+
+        // Draw MPG histogram bars
+        svgHistogramMPG.selectAll("rect")
+            .data(histogramMPGData)
+            .join("rect")
+            .attr("class", "non-brushed")
+            .attr("x", d => xScaleHistMPG(d.x0))
+            .attr("y", d => yScaleHistMPG(d.length))
+            .attr("width", d => xScaleHistMPG(d.x1) - xScaleHistMPG(d.x0))
+            .attr("height", d => height - yScaleHistMPG(d.length));
 
     })
     .catch(function (err) {
@@ -121,23 +197,86 @@ d3.csv("cars.csv")
 
 // helper functions
 // formats data for bar chart
-function getBarData(data) {
-    let returnData = [];
+function deepCopy(inObject) { // inclass: add
+    let outObject, value, key;
+    if (typeof inObject !== "object" || inObject === null) {
+        return inObject; // Return the value if inObject is not an object
+    }
+    // Create an array or object to hold the values
+    outObject = Array.isArray(inObject) ? [] : {};
+    for (key in inObject) {
+        value = inObject[key];
+        // Recursively (deep) copy for nested objects, including arrays
+        outObject[key] = deepCopy(value);
+    }
+    return outObject;
+}
 
-    data.forEach((obj) => {
-        let uniqueCyl = returnData.reduce((prev, curr) => (prev && curr.cyl != obj.cyl), true);
-        if (uniqueCyl) {
-            returnData.push({
-                "cyl": +obj.cyl,
-                "count": 1
-            });
-        } else {
-            let cylIdx = returnData.findIndex((elem) => elem.cyl == +obj.cyl);
-            returnData[cylIdx].count++;
+function brushFxn(event) { // inclass: add
+    // console.log(event);
+
+    // revert points to initial style
+    points.attr("class", "non-brushed");
+
+    let brushCoords;
+    if (event.selection != null) {
+        let brushCoordsD3 = d3.brushSelection(this);
+        brushCoords = {
+            "x0": brushCoordsD3[0][0],
+            "x1": brushCoordsD3[1][0],
+            "y0": brushCoordsD3[0][1],
+            "y1": brushCoordsD3[1][1]
         }
-    });
-    returnData = returnData.sort((a, b) => a.cyl - b.cyl);
-    // console.log(returnData);
 
-    return returnData;
+        // style brushed points
+        points.filter(brushFilter)
+            .attr("class", "brushed");
+        
+        // filter bar data
+        let filteredScatterData = scatterData.filter(brushFilter);
+        filteredHistogramHPData = getHistogramData(filteredScatterData, 'hp', 20);
+        filteredHistogramMPGData = getHistogramData(filteredScatterData, 'mpg', 20);
+        
+        // render bars in real time
+        updateHistograms();
+    }
+
+    function brushFilter(d) {
+        // iterating over data bound to my points
+        let cx = xScaleScatter(d.hp),
+            cy = yScaleScatter(d.mpg);
+
+        // get only points inside of brush
+        return (brushCoords.x0 <= cx && brushCoords.x1 >= cx && brushCoords.y0 <= cy && brushCoords.y1 >= cy);
+    }
+}
+
+function updateHistograms() {
+    // Update HP histogram
+    let yScaleHistHP = d3.scaleLinear()
+        .domain([0, d3.max(filteredHistogramHPData, d => d.length)])
+        .range([height, 0]);
+
+    svgHistogramHP.selectAll("rect.brushed")
+        .data(filteredHistogramHPData)
+        .join("rect")
+        .attr("class", "brushed")
+        .attr("x", d => xScaleHistHP(d.x0))
+        .attr("y", d => yScaleHistHP(d.length))
+        .attr("width", d => xScaleHistHP(d.x1) - xScaleHistHP(d.x0))
+        .attr("height", d => height - yScaleHistHP(d.length));
+
+    // Update MPG histogram
+    let yScaleHistMPG = d3.scaleLinear()
+        .domain([0, d3.max(filteredHistogramMPGData, d => d.length)])
+        .range([height, 0]);
+
+    svgHistogramMPG.selectAll("rect.brushed")
+        .data(filteredHistogramMPGData)
+        .join("rect")
+        .attr("class", "brushed")
+        .attr("x", d => xScaleHistMPG(d.x0))
+        .attr("y", d => yScaleHistMPG(d.length))
+        .attr("width", d => xScaleHistMPG(d.x1) - xScaleHistMPG(d.x0))
+        .attr("height", d => height - yScaleHistMPG(d.length));
 }
